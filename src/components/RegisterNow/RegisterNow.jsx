82712639406c3ec90aae8f0adb2942e04f696929
devmohash <engmohamedashraf2016@gmail.com>
2023-11-12 22:52:35 +0300

    some modifications

--- src/components/RegisterNow/RegisterNow.jsx
+++ src/components/RegisterNow/RegisterNow.jsx
@@ -1,23 +1,29 @@
 import { Link } from "react-router-dom";
 import runcat from "../../assets/runningcat.jpg";
+import "./RegisterNow.css";
+
 const RegisterNow = () => {
   return (
     <section className="registernow py-5 bg-warning-subtle">
       <div className="container">
         <div className="row rounded shadow bg-light">
-          <div className="col-4 p-0">
-            <img
-              className="w-100 rounded-start"
-              src={runcat}
-              alt="catrunning"
-            />
+          <div className="col-lg-4 col-12 p-0">
+            <div>
+              <img
+                className="w-100 rounded-start"
+                src={runcat}
+                alt="catrunning"
+              />
+            </div>
           </div>
-          <div className="col-8 py-5 ps-5">
-            <h3 className="text-warning fw-bold fs-1 mb-5">Stop Running !</h3>
-            <h3 className="mb-5 fs-2">You got a place here... </h3>
-            <Link className="btn btn-primary fw-bold" to="/signup">
-              Join Us Now
-            </Link>
+          <div className="col-lg-8 col-12 py-5 ps-5">
+            <div>
+              <h3 className="text-warning fw-bold fs-1 mb-5">Stop Running !</h3>
+              <h3 className="mb-5 fs-2">You got a place here... </h3>
+              <Link className="btn btn-primary fw-bold" to="/signup">
+                Join Us Now
+              </Link>
+            </div>
           </div>
         </div>
       </div>

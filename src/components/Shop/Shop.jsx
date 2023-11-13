bfd993caa2b4e78ef91665f59f092306d78bc638
OssamaZahw <142718611+OssamaZahw@users.noreply.github.com>
2023-11-13 06:52:43 +0200

    shop structure

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import ShopItemsContainer from './ShopItemsContainer/ShopItemsContainer'
+import useGetShopItems from '../../hooks/useGetShopItems'
 
 function Shop() {
+
+  const [loading, error, apiArr] = useGetShopItems();
+  const [defaultArr, setDefaultArr] = useState([]); // An Array to keep the default API response
+  const [displayArr, setDisplayArr] = useState([]); // An Array that can be manipulated to be rendered
+
+  useEffect(() => {
+    setDefaultArr(apiArr);
+    
+  }, [loading])
+
+  // useEffect(()=> {
+  //   console.log(defaultArr);
+  // },[defaultArr])
+  
+
   return (
     <>
     <div>Shop</div>
--- src/hooks/useGetShopItems.js
+++ src/hooks/useGetShopItems.js
@@ -4,14 +4,27 @@ import { axiosShop } from "../services/axiosShopConfig";
 
 const useGetShopItems = (param) => {
     const [itemsArr, setItemsArr] = useState([]);
-    const [loading, setLoading] = useState (true);
-    const [error, setError] = useState (false)
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(false)
 
     const loadItems = async () => {
         try {
-            const res = await axiosShop
-        } catch {}
-    }
-}
+            const res = await axiosShop.get(param);
+            // console.log(res)
+            setItemsArr(res.data);
+            setLoading(false);
+        } catch (error) {
+            console.log(error);
+            // setError here
+            setLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        loadItems()
+    }, [param])
+
+    return [loading, error, itemsArr]
+};
 
 export default useGetShopItems

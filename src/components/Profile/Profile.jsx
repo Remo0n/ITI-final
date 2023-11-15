b0e2409040cd4c9851da3f4c42800f84bce3b0a9
omar adel <elhwety5555@gmail.com>
2023-11-16 00:22:37 +0200

    get profile data

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,19 +1,26 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { db } from "../../services/firebaseConfig";
 import { doc, setDoc, updateDoc, arrayUnion, getDoc } from "firebase/firestore";
+import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
 import "./Profile.css";
 
 export const Profile = () => {
   const [pet, setPet] = useState({
     name: "",
-    age: 0,
-    breed: "",
+    age: "",
     color: "",
-    description: "",
+    status: "",
     image: null,
   });
+
+  const [petProfiles, setPetProfiles] = useState([]);
+  console.log(petProfiles);
   const { user } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    getPetProfiles(user.uid);
+  }, [user.uid]);
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
@@ -33,33 +40,74 @@ export const Profile = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const userRef = doc(db, "users", user.uid);
-
     try {
-      // Get the current user document
+      // Upload image to Firebase Storage
+      if (pet.image) {
+        const imageUrl = await uploadImage(pet.image);
+        // Set the image URL directly in the pet state
+        setPet((prevPet) => ({
+          ...prevPet,
+          image: imageUrl,
+        }));
+      }
+
+      const userRef = doc(db, "users", user.uid);
+
       const docSnap = await getDoc(userRef);
+
       if (docSnap.exists()) {
-        // User document exists, update it
         await updateDoc(userRef, {
           petProfiles: arrayUnion(pet),
         });
+        setPetProfiles((prevProfiles) => [...prevProfiles, pet]);
       } else {
-        // User document does not exist, create a new one
         await setDoc(userRef, {
           petProfiles: [pet],
         });
+        setPetProfiles([pet]);
       }
     } catch (error) {
-      console.log(error);
+      console.error("Error handling form submission:", error);
     }
   };
+
+  async function uploadImage(file) {
+    const storage = getStorage();
+    const storageRef = ref(storage, `images/${file.name}`);
+
+    try {
+      const snapshot = await uploadBytes(storageRef, file);
+      const url = await getDownloadURL(snapshot.ref);
+      return url;
+    } catch (error) {
+      console.error("Error uploading image: ", error);
+      return null;
+    }
+  }
+
+  async function getPetProfiles(userId) {
+    const userRef = doc(db, "users", userId);
+
+    try {
+      const docSnap = await getDoc(userRef);
+
+      if (docSnap.exists()) {
+        const userData = docSnap.data();
+        setPetProfiles(userData.petProfiles || []);
+      } else {
+        console.log("No such user!");
+      }
+    } catch (error) {
+      console.error("Error getting pet profiles: ", error);
+    }
+  }
 
   return (
     <div className="pet-profile">
       <div className="pet-profile__img-box">
         <label htmlFor="imageUpload">
           {pet.image ? (
-            <img src={URL.createObjectURL(pet.image)} alt="Pet" />
+            <img src={pet.image} alt="" />
           ) : (
             <span>Upload Image</span>
           )}
@@ -71,42 +119,38 @@ export const Profile = () => {
           id="imageUpload"
         />
       </div>
-
       <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          name="name"
-          placeholder="Name"
-          value={pet.name}
-          onChange={handleInputChange}
-        />
-        <input
-          type="number"
-          name="age"
-          placeholder="Age"
-          value={pet.age}
-          onChange={handleInputChange}
-        />
-        <input
-          type="text"
-          name="breed"
-          placeholder="Breed"
-          value={pet.breed}
-          onChange={handleInputChange}
-        />
-        <input
-          type="text"
-          name="color"
-          placeholder="Color"
-          value={pet.color}
-          onChange={handleInputChange}
-        />
-        <textarea
-          name="description"
-          placeholder="Description"
-          value={pet.description}
-          onChange={handleInputChange}
-        ></textarea>
+        <div className="form-box">
+          <input
+            type="text"
+            name="name"
+            placeholder="Name"
+            value={pet.name}
+            onChange={handleInputChange}
+          />
+          <input
+            type="number"
+            name="age"
+            placeholder="Age"
+            value={pet.age}
+            onChange={handleInputChange}
+          />
+          <input
+            type="text"
+            name="color"
+            placeholder="Color"
+            value={pet.color}
+            onChange={handleInputChange}
+          />
+          <textarea
+            rows="2"
+            cols="50"
+            name="status"
+            placeholder="status"
+            value={pet.status}
+            onChange={handleInputChange}
+          ></textarea>
+        </div>
         <button type="submit">Submit</button>
       </form>
     </div>

0b38a06cedabde89757bbe01bd13931344ad3ff6
Remon <remoon.peter@gmail.com>
2023-11-19 21:40:48 +0200

    fix typo

--- src/components/OurServices/OurServices.jsx
+++ src/components/OurServices/OurServices.jsx
@@ -8,20 +8,20 @@ const Services = () => {
     en: [
       {
         id: 1,
-        serviceFigure: "src/assets/ser1.jpg",
+        serviceFigure: "/assets/ser1.jpg",
         serviceTitle: "Find Your Vet",
         serviceDesc: "Find & choose your nearest vet clinic.  ",
       },
       {
         id: 2,
-        serviceFigure: "src/assets/ser2.png",
+        serviceFigure: "/assets/ser2.png",
         serviceTitle: "Find Your Shop",
         serviceDesc:
           "Find & choose your nearest shop for any supplies you need for your pet. ",
       },
       {
         id: 3,
-        serviceFigure: "src/assets/ser3.png",
+        serviceFigure: "/assets/ser3.png",
         serviceTitle: "Register Your Pet Profile",
         serviceDesc:
           "You can create a profile for your pet and save all its information.",
@@ -30,20 +30,20 @@ const Services = () => {
     ar: [
       {
         id: 1,
-        serviceFigure: "src/assets/ser1.jpg",
+        serviceFigure: "/assets/ser1.jpg",
         serviceTitle: "ابحث عن عيادتك البيطرية",
         serviceDesc: "ابحث واختر عيادتك البيطرية الأقرب اليك  ",
       },
       {
         id: 2,
-        serviceFigure: "src/assets/ser2.png",
+        serviceFigure: "/assets/ser2.png",
         serviceTitle: "ابحث عن متجرك ",
         serviceDesc:
           "ابحث واختر المتجر الأقرب اليك من أجل مستلزمات حيوانك الأليف ",
       },
       {
         id: 3,
-        serviceFigure: "src/assets/ser3.png",
+        serviceFigure: "/assets/ser3.png",
         serviceTitle: "سجل حساب لحيوانك الأليف",
         serviceDesc: "سجل حساب لحيوانك الأليف واحتفظ بجميع معلوماته التي تهمك",
       },
